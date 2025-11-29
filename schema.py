462882fe78705579bc3f935b5440cec415a32580
55408suga <55408suga.ka@gmail.com>
2025-11-29 19:01:20 +0900

    feat: 初期APIとデータベースセットアップ (Todo管理機能)

--- schema.py
+++ schema.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema,fields
 
+
 class PlainTodoSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
@@ -7,3 +8,12 @@ class PlainTodoSchema(Schema):
     updated_at = fields.DateTime(dump_only=True)
     deadline = fields.DateTime(allow_none=True)
     is_done = fields.Bool(load_default=False)
+
+
+class TodoUpdateSchema(Schema):
+    name = fields.Str()
+    deadline = fields.DateTime(allow_none=True)
+    is_done = fields.Bool()
+
+class TodoSchema(PlainTodoSchema):
+    pass
