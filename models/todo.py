ec23f83afcd86bfb4b841668edd183711caea6b7
55408suga <55408suga.ka@gmail.com>
2025-11-29 16:16:53 +0900

    feat:todoのスキーマとモデルを作成

---
+++ models/todo.py
@@ -0,0 +1,12 @@
+from db import db
+from sqlalchemy.sql import func
+
+class TodoModel(db.Model):
+    __tablename__ = "todos"
+
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(80),nullable=False)
+    created_at = db.Column(db.DateTime,default=func.now())
+    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+    deadline = db.Column(db.DateTime,nullable=True)
+    is_done = db.Column(db.Boolean,default=False,nullable=False)
---
+++ schema.py
@@ -0,0 +1,9 @@
+from marshmallow import Schema,fields
+
+class PlainTodoSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    deadline = fields.DateTime(allow_none=True)
+    is_done = fields.Bool(load_default=False)
