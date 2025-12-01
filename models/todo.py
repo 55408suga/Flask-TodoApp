0078dee32038849f945378babde2cbf09d48cad1
55408suga <55408suga.ka@gmail.com>
2025-12-01 20:37:53 +0900

    style: Blackによるコード整形

--- models/todo.py
+++ models/todo.py
@@ -1,13 +1,14 @@
 from db import db
 from sqlalchemy.sql import func
 
+
 class TodoModel(db.Model):
     __tablename__ = "todos"
 
-    id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(80),nullable=False)
-    created_at = db.Column(db.DateTime,default=func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, default=func.now())
     updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
-    deadline = db.Column(db.DateTime,nullable=True)
-    is_done = db.Column(db.Boolean,default=False,nullable=False)
-    tags =db.relationship("TagModel",back_populates="todos",secondary="todo_tags")
+    deadline = db.Column(db.DateTime, nullable=True)
+    is_done = db.Column(db.Boolean, default=False, nullable=False)
+    tags = db.relationship("TagModel", back_populates="todos", secondary="todo_tags")
--- models/todo_tags.py
+++ models/todo_tags.py
@@ -1,8 +1,9 @@
-from db import db 
+from db import db
+
 
 class TodoTags(db.Model):
     __tablename__ = "todo_tags"
 
-    id = db.Column(db.Integer,primary_key=True)
-    tag_id = db.Column(db.Integer,db.ForeignKey("tags.id"),nullable=False)
-    todo_id = db.Column(db.Integer,db.ForeignKey("todos.id"),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.id"), nullable=False)
+    todo_id = db.Column(db.Integer, db.ForeignKey("todos.id"), nullable=False)
