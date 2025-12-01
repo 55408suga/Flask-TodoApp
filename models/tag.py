988f45d70fd5f7fb3f769503b243b61f5f509a1f
55408suga <55408suga.ka@gmail.com>
2025-12-01 12:37:13 +0900

    feat: tag付け機能を追加

--- models/tag.py
+++ models/tag.py
@@ -4,5 +4,5 @@ class TagModel(db.Model):
     __tablename__ = "tags"
 
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Colummn(db.String(80),nullable=False,unique=True)
-    todos = db.relationship("TodoModel",back_populates="tags",scondary="todo_tags")
+    name = db.Column(db.String(80),nullable=False,unique=True)
+    todos = db.relationship("TodoModel",back_populates="tags",secondary="todo_tags")
