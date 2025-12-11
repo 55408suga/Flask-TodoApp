da4d155ccbc970979aa2e2f304f2eba05d664b94
55408suga <55408suga.ka@gmail.com>
2025-12-11 16:32:33 +0900

    feat: jwtの設定

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask,jsonify
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from db import db
 import models
 from resources.todo import blp as TodoBlueprint
@@ -25,6 +26,58 @@ def create_app(db_url=None):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY","local-secret-key")
+    jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+    
     api = Api(app)
     api.register_blueprint(TodoBlueprint)
     api.register_blueprint(TagBlueprint)
